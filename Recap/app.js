5278005b48d6d03344bb1ba6a0b7379da84697f1
Monika Sikora <monikastanko90@gmail.com>
2023-06-28 14:23:12 +0200

    NodeJS recap - yargs commands

--- Recap/app.js
+++ Recap/app.js
@@ -1,63 +1,63 @@
-const chalk = require('chalk');
-const yargs = require('yargs');
-const getNotes = require('./notes.js');
+// const chalk = require('chalk');
+// const yargs = require('yargs');
+// const getNotes = require('./notes.js');
 
-// const command = process.argv[2];
+// // const command = process.argv[2];
 
-// if (command === 'add') {
-//     console.log('Adding note!');
-// } else if (command === 'remove') {
-//     console.log('Removing note!');
-// }
+// // if (command === 'add') {
+// //     console.log('Adding note!');
+// // } else if (command === 'remove') {
+// //     console.log('Removing note!');
+// // }
 
 
-//add
-yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        },
-    },
-    handler: function (argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Body: ' + argv.body);
-    }
-})
+// //add
+// yargs.command({
+//     command: 'add',
+//     describe: 'Add a new note',
+//     builder: {
+//         title: {
+//             describe: 'Note title',
+//             demandOption: true,
+//             type: 'string'
+//         },
+//         body: {
+//             describe: 'Note body',
+//             demandOption: true,
+//             type: 'string'
+//         },
+//     },
+//     handler: function (argv) {
+//         console.log('Title: ' + argv.title);
+//         console.log('Body: ' + argv.body);
+//     }
+// })
 
-//remove
-yargs.command({
-    command: 'remove',
-    describe: 'Removing a new note',
-    handler: function () {
-        console.log('Removing a new note...');
-    }
-})
+// //remove
+// yargs.command({
+//     command: 'remove',
+//     describe: 'Removing a new note',
+//     handler: function () {
+//         console.log('Removing a new note...');
+//     }
+// })
 
-//read
-yargs.command({
-    command: 'read',
-    describe: 'Reading a note',
-    handler: function () {
-        console.log('Reading a note...');
-    }
-})
+// //read
+// yargs.command({
+//     command: 'read',
+//     describe: 'Reading a note',
+//     handler: function () {
+//         console.log('Reading a note...');
+//     }
+// })
 
-//list
-yargs.command({
-    command: 'listd',
-    describe: 'Listing all notes',
-    handler: function () {
-        console.log('Listing all notes...');
-    }
-})
+// //list
+// yargs.command({
+//     command: 'listd',
+//     describe: 'Listing all notes',
+//     handler: function () {
+//         console.log('Listing all notes...');
+//     }
+// })
 
-yargs.parse();
+// yargs.parse();
