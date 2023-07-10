8cd8761cec732994e461d82ac867031619d3224e
Monika Sikora <monikastanko90@gmail.com>
2023-07-10 11:20:50 +0200

    React Crash Course - Adding React Router part 1

--- ReactCC/react-crash-course/src/components/PostsList.jsx
+++ ReactCC/react-crash-course/src/components/PostsList.jsx
@@ -1,35 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-import Post from "./Post";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import classes from "./PostsList.module.css";
+import Post from './Post';
+import classes from './PostsList.module.css';
 
-function PostsList({ isPosting, onStopPosting }) {
+function PostsList() {
+  // THIS WOULD CAUSE AN INFINITE LOOP! REACTS RELOADS!
+  // fetch("http://localhost:8080/posts")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     setPosts(data.posts);
+  //   });
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
-  function addPostHandler(postData) {
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  // function addPostHandler(postData) {
+  //   fetch('http://localhost:8080/posts', {
+  //     method: 'POST',
+  //     body: JSON.stringify(postData),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   setPosts((existingPosts) => [postData, ...existingPosts]);
+  // }
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
-        <div style={{ textAlign: "center", color: "white" }}>
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet!</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
--- ReactCC/react-crash-course/src/main.jsx
+++ ReactCC/react-crash-course/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Posts from './routes/Posts';
+import RootLayout from './routes/RootLayout';
+import NewPost from './routes/NewPost';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        children: [{ path: '/create', element: <NewPost /> }],
+      },
+    ],
+  },
+  // {
+  //   path: "*",
+  //   element: (
+  //     <div style={{ textAlign: "center", color: "white" }}>
+  //       <p>Route not known</p>
+  //     </div>
+  //   ),
+  // },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
